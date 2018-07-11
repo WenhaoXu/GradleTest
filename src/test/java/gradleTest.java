4032cfde0601748fafc5e927d7849a6ef61005cb
XUHO3 <howells.xu@oocl.com>
2018-07-11 09:25:32 +0800

    gradle test

---
+++ src/test/java/gradleTest.java
@@ -0,0 +1,20 @@
+import org.junit.Test;
+
+import java.util.LinkedList;
+import static org.mockito.Mockito.*;
+import static org.junit.Assert.*;
+
+public class gradleTest {
+    @Test
+    public void testMockClass() throws Exception {
+        // you can mock concrete classes, not only interfaces
+        LinkedList mockedList = mock(LinkedList.class);
+
+        // stubbing appears before the actual execution
+        String value = "first";
+        when(mockedList.get(0)).thenReturn(value);
+
+        assertEquals(mockedList.get(0), value);
+
+    }
+}
